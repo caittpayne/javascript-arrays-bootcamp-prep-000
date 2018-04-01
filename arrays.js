7443b7db9b95bebd3207444f60019962251f9b90
Cait Payne <caitpayne@outlook.com>
2018-04-01 23:12:03 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,10 +6,10 @@ var chocolateBars = [
   ]
   
 function addElementToBeginningOfArray(array, element) {
-  return [element, ...array]
+  return [element, ...array];
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  return array.unshift(element)
+  return array.unshift(element);
   
 }
