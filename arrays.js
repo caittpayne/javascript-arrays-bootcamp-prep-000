b2bdbd6251ffc28d5c3667f86284cfa4268dd3a8
Cait Payne <caitpayne@outlook.com>
2018-04-01 22:47:33 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,13 @@
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles'
+  ]
+  
+  var cities = [
+    'New York', 
+    'San Francisco'
+    ]
+    
+  console.log(["philadelphia", ...cities])
